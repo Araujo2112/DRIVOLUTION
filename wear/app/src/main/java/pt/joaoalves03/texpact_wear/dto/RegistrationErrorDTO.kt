f785928e1eac5373b6374e1990681e4f9ed8746b
Tiago Araujo <a.tiago@ipvc.pt>
2026-05-28 00:48:32 +0100

    delete none use

--- wear/app/src/main/java/pt/joaoalves03/texpact_wear/dto/RegistrationErrorDTO.kt
+++
@@ -1,6 +0,0 @@
-package pt.joaoalves03.texpact_wear.dto
-
-data class RegistrationErrorDTO(
-    val message: String,
-    val name: String
-)
--- wear/app/src/main/java/pt/joaoalves03/texpact_wear/dto/UserStatusDTO.kt
+++
@@ -1,9 +0,0 @@
-package pt.joaoalves03.texpact_wear.dto
-
-import com.fasterxml.jackson.annotation.JsonInclude
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class UserStatusDTO(
-  val heartRate: Float? = 0f,
-  val steps: Int? = 0
-)
